af8effd171fbbe277d6f8a3ca3e82c9364d6cdc4
Liprinka <144840720+Liprinka@users.noreply.github.com>
2024-01-12 01:42:08 +0300

    Update index.js
    Tarrif.jsx in index.js

--- drupal/src/index.js
+++ drupal/src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Tariff from './Tariff';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />   
+//   </React.StrictMode>
+// );
+
+const tariff = ReactDOM.createRoot(document.getElementById('tariff'));
+tariff.render(
+   <React.StrictMode>
+     <Tariff />   
+   </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
